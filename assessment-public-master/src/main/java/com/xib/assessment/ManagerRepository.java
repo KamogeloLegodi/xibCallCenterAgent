796d709a0dff4c1ec61ce8ea9abe7e32e251c2f3
Kamogelo.Legodi <kamogelo.legodi@HITASP.com>
2020-04-24 20:40:49 +0200

    removed duplicate files

--- assessment-public-master/src/main/java/com/xib/assessment/ManagerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xib.assessment;
-
-import com.xib.assessment.Manager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerRepository extends JpaRepository<Manager, Long> {
-}
